c2a02a715b98848bfabc3c2c50fc247de1dd1656
yagizhanm <80545246+yagizhanm@users.noreply.github.com>
2021-04-08 16:23:39 +0300

    Update HW2.py
    This is a code that prints couple of CV's having the form dictionary.

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,13 @@
 #Explain your work
 
 #Question 1
-for x in range(a):
-	print(a)
+CV1 = dict() # Creating an empy dictionary.
+CV1 = {"Name":"Yağızhann","Surname " : "Macuncu ", "University " : "METU"}
+CV2 = {"Name":"HASAN ","Surname " : "KAYA ", "University " : "OXFORD"}
+CV3 = {"Name":"MAHMUT ALİ ","Surname " : "ÇAKIR ", "University " : "ITU"}
+CV4 = {"Name":"ABRAHAM ","Surname " : "LINCOLN ", "University " : "MIT"}
+CV5 = {"Name":"FENERBAHÇELİ  ","Surname " : "CEMİL ", "University " : "FENERBAHCE"} # 5 CV'S with a couple of property.
+cv = list()  # Creating an empty list so that we can store our CVi's such that i€{1,2,...,5}.
+cv = [CV1, CV2, CV3, CV4, CV5] # Inserting CVi's into the list 'cv'.
+for k in cv: # Printing the CVi's which are replaced in the list cv.
+    print(k)
